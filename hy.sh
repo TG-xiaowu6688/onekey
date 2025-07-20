68573f3b63ef5fceb291c9141c8a34f6248cf6fc
@xiaowu6688 <jzkj6688@icloud.com>
2025-07-20 19:21:54 +0800

    Delete hy.sh

--- hy.sh
+++
@@ -1,9 +0,0 @@
-bash <(curl -sSL https://linuxmirrors.cn/main.sh) \
-  --source mirrors.aliyun.com \
-  --protocol http \
-  --use-intranet-source false \
-  --install-epel true \
-  --backup true \
-  --upgrade-software false \
-  --clean-cache false \
-  --ignore-backup-tips
